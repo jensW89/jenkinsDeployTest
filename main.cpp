a257b8d52e94a97d7cef88835a9f9eebc4218c50
jwambach <wambach@sigma3d.de>
2016-12-02 11:32:06 +0100

    change, to see jenkins working

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qDebug() << "Hello jenkins from the qt c consol programm!";
+    qDebug() << "Hello jenkins from the qt c consol programm!";
     return a.exec();
 }
