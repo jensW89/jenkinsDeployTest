ef23084677b771eee40debc22d1ef9fa6301b8e7
jwambach <wambach@sigma3d.de>
2016-12-02 11:40:18 +0100

    changes for running

--- main.cpp
+++ main.cpp
@@ -5,6 +5,5 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qDebug() << "Hello jenkins from the qt c consol programm!";
-    qDebug() << "Hello jenkins from the qt c consol programm!";
     return a.exec();
 }
